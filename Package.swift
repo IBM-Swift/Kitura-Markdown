c9dafe916748018dba38d7a04e137f0834c2791e
David Dunn <davdunn2@uk.ibm.com>
2018-05-14 15:21:38 +0100

    Remove comments and Slack notif

--- Package.swift
+++ Package.swift
@@ -22,7 +22,6 @@ import PackageDescription
 let package = Package(
     name: "KituraMarkdown",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "KituraMarkdown",
             targets: ["KituraMarkdown"]
@@ -32,8 +31,6 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", .branch("master"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Ccmark"
         ),
